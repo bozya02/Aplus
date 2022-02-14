f295ca9c2a559298a32c553c2998b4fd11d5b0c4
HaGaDDD <bozinandrey1@yandex.ru>
2022-02-14 14:53:06 +0300

    Complete Edit page

--- Aplus/Aplus/Pages/PageEditProject.xaml.cs
+++ Aplus/Aplus/Pages/PageEditProject.xaml.cs
@@ -14,9 +14,49 @@ namespace Aplus
 	[XamlCompilation(XamlCompilationOptions.Compile)]
 	public partial class PageEditProject : ContentPage
 	{
-		public PageEditProject(Project project)
+        public Project project { get; set; }
+		public PageEditProject(Project newProject)
 		{
 			InitializeComponent();
+            project = newProject;
 		}
-	}
+
+        private async void tbDeleteClicked(object sender, EventArgs e)
+        {
+            bool result = await DisplayAlert("", $"Вы точно хотите удалить {project.Name}", "Удалить", "Отмена");
+
+            if (result)
+            {
+                App.Database.DeleteProject(project.Id);
+                await Navigation.PushAsync(new PageProjects());
+            }
+        }
+
+        private async void btnEditClicked(object sender, EventArgs e)
+        {
+            bool result = await DisplayAlert("", $"Вы точно хотите изменить {project.Name}", "Изменить", "Отмена");
+            
+            Project newProject = new Project
+            {
+                Id = project.Id,
+                Name = entryName.Text,
+                Description = entryDesc.Text,
+                Phone = entryPhone.Text,
+                Email = entryEmail.Text,
+                Address = entryAddress.Text
+            };
+            
+            if (result)
+            {
+                App.Database.AddProject(project);
+                await Navigation.PushAsync(new PageProjects());
+            }
+            
+        }
+
+        private async void btnCancelClicked(object sender, EventArgs e)
+        {
+            await Navigation.PopAsync();
+        }
+    }
 }
